a7263501fe27202d7585134c2e37a9b7bf13f65f
Maria Bakaeva <mary396969@gmail.com>
2020-04-20 11:25:32 +0300

    Update path to bin

--- Bakaeva_tomogram_visualizer/Bakaeva_tomogram_visualizer/main.cpp
+++ Bakaeva_tomogram_visualizer/Bakaeva_tomogram_visualizer/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     View w(NULL);
-    w.LoadData("C:\\Users\\mary396969\\Desktop\\cg\\Computer-Graphic\\Bakaeva_tomogram_visualizer\\Bakaeva_tomogram_visualizer\\data\\testdata.bin");
+    w.LoadData("..\\Bakaeva_tomogram_visualizer\\data\\testdata.bin");
     w.show();
     return a.exec();
 }
